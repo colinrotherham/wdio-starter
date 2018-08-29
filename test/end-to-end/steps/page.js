9807a499de815e85dfb15b106ee57d5276cdda1a
Colin Rotherham <work@colinr.com>
2018-08-29 11:03:48 +0100

    Upgrade to CucumberJS v4

--- test/end-to-end/steps/page.js
+++ test/end-to-end/steps/page.js
@@ -1,26 +1,23 @@
+import { Given, When, Then } from 'cucumber';
 import chai from 'chai';
-import { defineSupportCode } from 'cucumber';
 import Page from '../pages/page';
 
 let expect = chai.expect;
 let page = new Page();
 
-defineSupportCode(({ Given, When, Then }) => {
+Given(/^I go to the home page$/, () => {
+    return page.goHome();
+});
 
-    Given(/^I go to the home page$/, () => {
-        return page.goHome();
-    });
+Given(/^I visit the URL '([^']*)?'$/, (matchHref) => {
+    return page.goToUrl(matchHref);
+});
 
-    Given(/^I visit the URL '([^']*)?'$/, (matchHref) => {
-        return page.goToUrl(matchHref);
-    });
+When(/^I click the link '([^']*)?'$/, (matchLink) => {
+    return browser.click(`a=${matchLink}`);
+});
 
-    When(/^I click the link '([^']*)?'$/, (matchLink) => {
-        return browser.click(`a=${matchLink}`);
-    });
-
-    Then(/^I should see the title '([^']*)?'$/, async (matchTitle) => {
-        let title = await page.getTitle();
-        expect(title).to.equal(matchTitle);
-    });
+Then(/^I should see the title '([^']*)?'$/, async (matchTitle) => {
+    let title = await page.getTitle();
+    expect(title).to.equal(matchTitle);
 });
