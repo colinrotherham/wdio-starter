ca07da7100486c49b84ddc870200dac4077bb48a
Colin Rotherham <work@colinr.com>
2019-05-21 08:33:42 +0100

    Add Babel core-js version

--- .babelrc.js
+++ .babelrc.js
@@ -3,6 +3,7 @@ module.exports = {
         ['@babel/env', {
             shippedProposals: true,
             useBuiltIns: 'usage',
+            corejs: '3.1.1',
             targets: {
                 browsers: [],
                 node: true,
